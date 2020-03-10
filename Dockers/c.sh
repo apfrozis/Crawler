35aa80b1543dd12907da4d3a57b8d662d625a7fd
Vitor Viana <masterviana@gmail.com>
2020-03-10 00:00:46 +0000

    get repo private in docker

---
+++ Dockers/c.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+#wget -O server_setup.sh https://raw.githubusercontent.com/samirtendulkar/my_project/master/deploy/server_setup.sh?token=AkSv7SycSHacUNlSEZamo6hpMAI6ZhsLks5b4uFuwA%3D%3D
+
+#https://superuser.com/questions/735452/how-can-i-download-a-private-repository-from-github-having-no-access-to-git-on
+
+
+#https://stackoverflow.com/questions/27214516/wget-other-users-private-repository-on-github
+
+#retira a master branch
+curl -u a5a5e0208c9e2fc0225805e4632820bdee9e178b:x-oauth-basic -L https://github.com/apfrozis/Crawler/tarball/master | tar xz
